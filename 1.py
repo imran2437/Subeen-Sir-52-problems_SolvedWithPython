1e2e78d23faa742d6c1c18d943dcde1263db8791
imran2437 <alimran689@gmail.com>
2023-08-19 21:54:09 +0600

    even

--- 1.py
+++ 1.py
@@ -1,24 +1,40 @@
-input=int(input("Enter the number:"))
-if input%2==0:
-    print("The number is even")
-else:
-    print("The number is Odd")
+n = int(input("Enter the number of integers: "))
+
+for _ in range(n):
+    a = int(input("Enter an integer: "))
+    if a % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+
+
+# input=int(input("Enter the number:"))
+# if input%2==0:
+#     print("The number is even")
+# else:
+#     print("The number is Odd")
     
+
+
+
+
 # using []
 # Create a list of integers
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# Initialize empty lists for even and odd numbers
-even_numbers = []
-odd_numbers = []
+# # Initialize empty lists for even and odd numbers
+# even_numbers = []
+# odd_numbers = []
 
-# Iterate through the numbers and classify them as even or odd
-for number in numbers:
-    if number % 2 == 0:
-        even_numbers.append(number)
-    else:
-        odd_numbers.append(number)
+# # Iterate through the numbers and classify them as even or odd
+# for number in numbers:
+#     if number % 2 == 0:
+#         even_numbers.append(number)
+#     else:
+#         odd_numbers.append(number)
 
-# Print the even and odd numbers
-print("Even numbers:", even_numbers)
-print("Odd numbers:", odd_numbers)
+# # Print the even and odd numbers
+# print("Even numbers:", even_numbers)
+# print("Odd numbers:", odd_numbers)
