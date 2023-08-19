4761c7676284ac458c6bde6878475c1565abf120
imran2437 <alimran689@gmail.com>
2023-08-19 21:17:24 +0600

    1

---
+++ 1.py
@@ -0,0 +1,24 @@
+input=int(input("Enter the number:"))
+if input%2==0:
+    print("The number is even")
+else:
+    print("The number is Odd")
+    
+# using []
+# Create a list of integers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Initialize empty lists for even and odd numbers
+even_numbers = []
+odd_numbers = []
+
+# Iterate through the numbers and classify them as even or odd
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        odd_numbers.append(number)
+
+# Print the even and odd numbers
+print("Even numbers:", even_numbers)
+print("Odd numbers:", odd_numbers)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Subeen-Sir-52-problems_SolvedWithPython
+#52 problems by Subeen Sir solved with python
